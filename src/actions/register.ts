7c70cbba5a5933f1091efde5c4ec79dadddf1bc3
Jayden Lee <jaydeh.lee@gmail.com>
2024-01-08 14:59:10 -0800

    register - create new user in db

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,6 +1,9 @@
 'use server'
-import { RegisterSchema } from '@/schemas'
+import { db } from '@/lib/db'
 import * as z from 'zod'
+import bcrypt from 'bcrypt'
+import { RegisterSchema } from '@/schemas'
+import { getUserByEmail } from '@/data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -8,6 +11,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: 'Invalid fields!'}
   }
+
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+  
+  const existingUser = await getUserByEmail(email)
+  
+  if (existingUser) return { error: 'Email already in use!'}
+
+    await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      }
+    })
+
+  // TOTO: Send verification token email
+
 
   return { success : 'Email sent!'}
 }
