7e1606c6b060afd6dc43ba53ab4397d5898bd4c6
Jayden Lee <jaydeh.lee@gmail.com>
2024-01-20 21:56:28 -0800

    OAuth config

--- src/routes.ts
+++ src/routes.ts
@@ -3,29 +3,24 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = [
-  '/'
-]
+export const publicRoutes = ["/"]
 
 /**
  * An array of routes that are used for authentication
  * These routes will redirect logged in users to /settings
  * @type {string[]}
  */
-export const authRoutes = [
-  '/auth/login',
-  '/auth/register'
-]
+export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"]
 
 /**
  * The prefix for API authentication routes
  * Routes that start with this prefix are used for API authentication purposes
  * @type {string}
  */
-export const apiAuthPrefix = '/api/auth'
+export const apiAuthPrefix = "/api/auth"
 
 /**
- * The default path after loggin in 
+ * The default path after loggin in
  * @type {string}
  */
-export const DEFAULT_LOGIN_REDIRECT = '/settings'
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
